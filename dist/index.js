8a74d6908f649da3d4f1477f6111eff0d4b0af27
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2022-11-19 20:44:30 -0300

    update

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
 "use strict";
 //INTRODUCCION
-function add(n1, n2, showResult, result) {
-    var operation = n1 + n2;
-    if (showResult) {
-        console.log(result + operation);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 2;
-var number2 = 4;
-var printResult = true;
-var phrase = "result is";
-var result = add(number1, number2, printResult, phrase);
-console.log(result);
+// function add(n1: number, n2: number, showResult: boolean, result: string) {
+//   const operation = n1 + n2;
+//   if (showResult) {
+//     console.log(result + operation);
+//   } else {
+//     return n1 + n2;
+//   }
+// }
+// const number1 = 2;
+// const number2 = 4;
+// const printResult = true;
+// const phrase = "result is";
+// const result = add(number1, number2, printResult, phrase);
+// console.log(result);
+//objetc types
